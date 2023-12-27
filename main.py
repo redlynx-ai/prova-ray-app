74a563af79968669a41c2666fd7108287416daeb
Alessandro Devo <alessandro.devo@gmail.com>
2023-12-27 13:12:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from ray import serve
 app = FastAPI()
 
 def cpu_intensive_task():
-    result = 0
-    for _ in range(1000000):
-        result += _ ** 2
-    return str(result)
+    for i in range(1000000):
+        yield f"{i ** 2}\n"  # Yielding each calculation result as a separate line
 
 @serve.deployment
 @serve.ingress(app)
 class FastAPIDeployment:
     @app.get("/hello")
     def say_hello(self, name: str):
-        result = cpu_intensive_task()  # Replace with a CPU-intensive operation
-        return StreamingResponse(result)
+        return StreamingResponse(cpu_intensive_task(), media_type="text/plain")
 
 prova = FastAPIDeployment.bind()
