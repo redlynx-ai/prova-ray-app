b883374416515e76bae7bbed1e794da05e2c0006
Alessandro Devo <alessandro.devo@gmail.com>
2024-01-04 22:12:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 from ray import serve
+import torch
 
 app = FastAPI()
 
-def cpu_intensive_task():
-    for i in range(1000000):
-        yield f"{i ** 2}\n"  # Yielding each calculation result as a separate line
+def gpu_intensive_task():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    for i in torch.arange(1000000, device=device):
+        yield f"{device} --> {(i ** 2).item()}\n"
 
 @serve.deployment
 @serve.ingress(app)
 class FastAPIDeployment:
     @app.get("/hello")
     def say_hello(self, name: str):
-        return StreamingResponse(cpu_intensive_task(), media_type="text/plain")
+        return StreamingResponse(gpu_intensive_task(), media_type="text/plain")
 
 prova = FastAPIDeployment.bind()
